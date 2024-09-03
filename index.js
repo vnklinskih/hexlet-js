6324206173b3eec3b88ba7c2f1fd9bf5455dc8e1
vnklinskih <vngamepost@gmail.com>
2024-09-04 00:20:52 +0300

    added prettier and conf file .prettierrc

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 // Пересечение массивов
 _.intersection([2, 1], [2, 3]); // => [2]
 
 // capitalize делает первую букву заглавной
-_.capitalize("hello"); // Hello
+_.capitalize('hello'); // Hello
 
-console.log("Hello, Hexlet!");
+console.log('Hello, Hexlet!');
 
 console.log(_.last(['one', 'two']));
