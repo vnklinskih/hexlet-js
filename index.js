7ffdb88c9745d0c290872a436797950e74cc7456
vnklinskih <vngamepost@gmail.com>
2024-09-03 15:40:29 +0300

    added code in index.js and package.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import _ from "lodash";
+
+// Пересечение массивов
+_.intersection([2, 1], [2, 3]); // => [2]
+
+// capitalize делает первую букву заглавной
+_.capitalize("hello"); // Hello
+
 console.log("Hello, Hexlet!");
+
+console.log(_.last(['one', 'two']));
